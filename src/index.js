684cdfea87ce547b55697e4b0604c75f33339af8
ivan-krizaj <ivan.krizaj@bb.agency>
2022-08-03 09:30:31 +0200

    updating index.js with button

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ class Button {
   static get toolbox() {
     return {
       icon: '<svg width="15" height="14" viewBox="0 0 15 14" xmlns="http://www.w3.org/2000/svg"><path d="M13.53 6.185l.027.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.569-1.568l4.838-4.837L6.396 2.23A1.125 1.125 0 1 1 7.986.64l5.52 5.518.025.027zm-5.815 0l.026.025a1.109 1.109 0 0 1 0 1.568l-5.644 5.644a1.109 1.109 0 1 1-1.568-1.568l4.837-4.837L.58 2.23A1.125 1.125 0 0 1 2.171.64L7.69 6.158l.025.027z" /></svg>',
-      title: "Quote",
+      title: "Button",
     };
   }
 
@@ -72,7 +72,7 @@ class Button {
    * @returns {string}
    */
   static get DEFAULT_QUOTE_PLACEHOLDER() {
-    return "Enter a quote";
+    return "Enter a link";
   }
 
   /**
@@ -105,7 +105,7 @@ class Button {
    * @returns {string}
    */
   static get DEFAULT_ALIGNMENT() {
-    return Quote.ALIGNMENTS.left;
+    return Button.ALIGNMENTS.left;
   }
 
   /**
@@ -175,13 +175,13 @@ class Button {
    *   readOnly - read-only mode flag
    */
   constructor({ data, config, api, readOnly }) {
-    const { ALIGNMENTS, DEFAULT_ALIGNMENT } = Quote;
+    const { ALIGNMENTS, DEFAULT_ALIGNMENT } = Button;
 
     this.api = api;
     this.readOnly = readOnly;
 
-    this.quotePlaceholder = config.quotePlaceholder || Quote.DEFAULT_QUOTE_PLACEHOLDER;
-    this.captionPlaceholder = config.captionPlaceholder || Quote.DEFAULT_CAPTION_PLACEHOLDER;
+    this.quotePlaceholder = config.quotePlaceholder || Button.DEFAULT_QUOTE_PLACEHOLDER;
+    this.captionPlaceholder = config.captionPlaceholder || Button.DEFAULT_CAPTION_PLACEHOLDER;
 
     this.data = {
       link: data.link || "",
